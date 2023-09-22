17267e4d55cd45546771e38ac5fa521d3fdfd4a3
krmkarakoc <121299807+devavcisi@users.noreply.github.com>
2023-09-22 11:43:14 +0300

    initialCommit

---
+++ kk/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace kk
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.isim = new System.Windows.Forms.TextBox();
+            this.soyad = new System.Windows.Forms.TextBox();
+            this.ekle = new System.Windows.Forms.Button();
+            this.yas = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.telno = new System.Windows.Forms.TextBox();
+            this.adres = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.sil = new System.Windows.Forms.Button();
+            this.güncelle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 196);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(771, 230);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick_1);
+       
+            // 
+            // isim
+            // 
+            this.isim.Location = new System.Drawing.Point(12, 33);
+            this.isim.Name = "isim";
+            this.isim.Size = new System.Drawing.Size(230, 20);
+            this.isim.TabIndex = 1;
+            // 
+            // soyad
+            // 
+            this.soyad.Location = new System.Drawing.Point(12, 72);
+            this.soyad.Name = "soyad";
+            this.soyad.Size = new System.Drawing.Size(230, 20);
+            this.soyad.TabIndex = 2;
+            // 
+            // ekle
+            // 
+            this.ekle.Location = new System.Drawing.Point(636, 33);
+            this.ekle.Name = "ekle";
+            this.ekle.Size = new System.Drawing.Size(119, 25);
+            this.ekle.TabIndex = 3;
+            this.ekle.Text = "ekle";
+            this.ekle.UseVisualStyleBackColor = true;
+            this.ekle.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // yas
+            // 
+            this.yas.Location = new System.Drawing.Point(12, 121);
+            this.yas.Name = "yas";
+            this.yas.Size = new System.Drawing.Size(230, 20);
+            this.yas.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "isim";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "soyisim";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "yas";
+            // 
+            // telno
+            // 
+            this.telno.Location = new System.Drawing.Point(265, 33);
+            this.telno.Name = "telno";
+            this.telno.Size = new System.Drawing.Size(230, 20);
+            this.telno.TabIndex = 8;
+            // 
+            // adres
+            // 
+            this.adres.Location = new System.Drawing.Point(265, 72);
+            this.adres.Name = "adres";
+            this.adres.Size = new System.Drawing.Size(230, 20);
+            this.adres.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(262, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "telefon numarası";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(262, 56);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "adres";
+            // 
+            // sil
+            // 
+            this.sil.Location = new System.Drawing.Point(636, 64);
+            this.sil.Name = "sil";
+            this.sil.Size = new System.Drawing.Size(113, 20);
+            this.sil.TabIndex = 12;
+            this.sil.Text = "sil";
+            this.sil.UseVisualStyleBackColor = true;
+            this.sil.Click += new System.EventHandler(this.sil_Click);
+            // 
+            // güncelle
+            // 
+            this.güncelle.Location = new System.Drawing.Point(636, 98);
+            this.güncelle.Name = "güncelle";
+            this.güncelle.Size = new System.Drawing.Size(113, 20);
+            this.güncelle.TabIndex = 13;
+            this.güncelle.Text = "güncelle";
+            this.güncelle.UseVisualStyleBackColor = true;
+            this.güncelle.Click += new System.EventHandler(this.güncelle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.güncelle);
+            this.Controls.Add(this.sil);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.adres);
+            this.Controls.Add(this.telno);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.yas);
+            this.Controls.Add(this.ekle);
+            this.Controls.Add(this.soyad);
+            this.Controls.Add(this.isim);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox isim;
+        private System.Windows.Forms.TextBox soyad;
+        private System.Windows.Forms.Button ekle;
+        private System.Windows.Forms.TextBox yas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox telno;
+        private System.Windows.Forms.TextBox adres;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button sil;
+        private System.Windows.Forms.Button güncelle;
+    }
+}
+
---
+++ kk/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity.Infrastructure;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kk
+{
+    public partial class Form1 : Form
+    {
+        DBTalepFormEntities db = new DBTalepFormEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.DenemeUser.ToList();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DenemeUser du = new DenemeUser();
+
+            du.Ad = isim.Text;
+            du.Soyad = soyad.Text;
+            du.Yaş=yas.Text;
+            du.Telno=telno.Text;
+            du.Adres=adres.Text;
+            db.DenemeUser.Add(du);
+            db.SaveChanges();
+            dataGridView1.DataSource= db.DenemeUser.ToList();
+
+           }
+        
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void güncelle_Click(object sender, EventArgs e)
+        {
+            int id= int.Parse(dataGridView1.CurrentRow.Cells[0].Value.ToString());
+
+            DenemeUser du = db.DenemeUser.FirstOrDefault(x=>x.ID==id);
+            du.Ad = isim.Text;
+            du.Soyad = soyad.Text;
+            du.Yaş = yas.Text;
+            du.Telno = telno.Text;
+            du.Adres = adres.Text;
+            db.SaveChanges();
+            dataGridView1.DataSource = db.DenemeUser.ToList();
+
+        }
+
+       
+
+      
+
+        private void dataGridView1_CellClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            isim.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            soyad.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+        }
+
+        private void sil_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value.ToString());
+            DenemeUser du = db.DenemeUser.FirstOrDefault(x=>x.ID==id);
+            DialogResult dr = new DialogResult();
+            dr = MessageBox.Show($@"{du.Ad}  {du.Soyad}    Personeli Silinecek 
+Personeli Kalıcı Olarak Silmek İstediğinize  eminmisiniz ? ", "Kalıcı Olarak Silinecek", MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+            if (dr == DialogResult.Yes)
+            {
+                db.DenemeUser.Remove(du);
+                db.SaveChanges();
+                dataGridView1.DataSource = db.DenemeUser.ToList();
+                isim.Text = soyad.Text = null;
+            }
+
+            
+        }
+    }
+}
